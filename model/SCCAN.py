95fa5910cdce3e2eae389839dcd1fd78d7cdc945
LTL <1350378249@qq.com>
2025-07-11 20:46:57 +0800

    convert model.SCCAN

--- model/SCCAN.py
+++ model/SCCAN.py
@@ -1,9 +1,9 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
+import jittor as jt
+from jittor import nn
+import jittor.nn as F
 
 from model.ASPP import ASPP
-from model.backbone_utils import Backbone
+from model.backbone_utils_jittor import Backbone
 from model.swin_sccan import SwinTransformer
 from model.loss import WeightedDiceLoss
 
@@ -11,9 +11,9 @@ from model.loss import WeightedDiceLoss
 def Weighted_GAP(supp_feat, mask):
     supp_feat = supp_feat * mask
     feat_h, feat_w = supp_feat.shape[-2:][0], supp_feat.shape[-2:][1]
-    area = F.avg_pool2d(mask, (supp_feat.size()[2], supp_feat.size()[3])) * feat_h * feat_w + 0.0005
-    supp_feat = F.avg_pool2d(input=supp_feat, kernel_size=supp_feat.shape[-2:]) * feat_h * feat_w / area
-    return supp_feat
+    area = mask.mean(dims=(2,3), keepdims=True) * feat_h * feat_w + 0.0005
+    supp_feat_mean = (supp_feat.sum(dims=(2,3), keepdims=True) / (area - 0.0005))
+    return supp_feat_mean
 
 
 class OneModel(nn.Module):
@@ -43,12 +43,12 @@ class OneModel(nn.Module):
             fea_dim = 1024 + 512
         self.down_query = nn.Sequential(
             nn.Conv2d(fea_dim, reduce_dim, kernel_size=1, padding=0, bias=False),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout2d(p=0.5)
         )
         self.down_supp = nn.Sequential(
             nn.Conv2d(fea_dim, reduce_dim, kernel_size=1, padding=0, bias=False),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout2d(p=0.5)
         )
 
@@ -77,24 +77,24 @@ class OneModel(nn.Module):
         self.ASPP_meta = ASPP(scale * embed_dim)
         self.res1_meta = nn.Sequential(
             nn.Conv2d(scale * embed_dim * 5, reduce_dim, kernel_size=1, padding=0, bias=False),
-            nn.ReLU(inplace=True)
+            nn.ReLU()
         )
         self.res2_meta = nn.Sequential(
             nn.Conv2d(reduce_dim, reduce_dim, kernel_size=3, padding=1, bias=False),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Conv2d(reduce_dim, reduce_dim, kernel_size=3, padding=1, bias=False),
-            nn.ReLU(inplace=True)
+            nn.ReLU()
         )
         self.cls_meta = nn.Sequential(
             nn.Conv2d(reduce_dim, reduce_dim, kernel_size=3, padding=1, bias=False),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Dropout2d(p=0.1),
             nn.Conv2d(reduce_dim, self.classes, kernel_size=1)
         )
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
 
     def get_optim(self, model, args, LR):
-        optimizer = torch.optim.SGD(
+        optimizer = jt.optim.SGD(
             [
                 {'params': model.down_query.parameters()},
                 {'params': model.down_supp.parameters()},
@@ -105,7 +105,7 @@ class OneModel(nn.Module):
                 {'params': model.res2_meta.parameters()},
                 {'params': model.cls_meta.parameters()},
             ], lr=LR, momentum=args.momentum, weight_decay=args.weight_decay)
-        optimizer_swin = torch.optim.AdamW(
+        optimizer_swin = jt.optim.AdamW(
             [
                 {"params": [p for n, p in model.named_parameters() if "transformer" in n and p.requires_grad]}
             ], lr=6e-5
@@ -117,11 +117,11 @@ class OneModel(nn.Module):
             param.requires_grad = False
 
     def generate_prior(self, query_feat_high, final_supp_list, mask_list, fts_size):
-        bsize, ch_sz, sp_sz, _ = query_feat_high.size()[:]
+        bsize, ch_sz, sp_sz, _ = query_feat_high.size()
         corr_query_mask_list = []
         cosine_eps = 1e-7
         for i, tmp_supp_feat in enumerate(final_supp_list):
-            resize_size = tmp_supp_feat.size(2)
+            resize_size = tmp_supp_feat.size()[2]
             tmp_mask = F.interpolate(mask_list[i], size=(resize_size, resize_size), mode='bilinear', align_corners=True)
 
             tmp_supp_feat = tmp_supp_feat
@@ -129,29 +129,28 @@ class OneModel(nn.Module):
             s = tmp_supp_feat.flatten(2).transpose(-2, -1)
 
             tmp_query = q
-            tmp_query = tmp_query.contiguous().permute(0, 2, 1)  # [bs, c, h*w]
-            tmp_query_norm = torch.norm(tmp_query, 2, 1, True)
+            tmp_query = tmp_query.permute(0, 2, 1)  # [bs, c, h*w]
+            tmp_query_norm = jt.norm(tmp_query, dim=1, keepdims=True)
 
             tmp_supp = s
-            tmp_supp = tmp_supp.contiguous()
-            tmp_supp_norm = torch.norm(tmp_supp, 2, 2, True)
+            tmp_supp_norm = jt.norm(tmp_supp, dim=2, keepdims=True)
 
-            similarity = torch.bmm(tmp_supp, tmp_query) / (torch.bmm(tmp_supp_norm, tmp_query_norm) + cosine_eps)
+            similarity = jt.bmm(tmp_supp, tmp_query) / (jt.bmm(tmp_supp_norm, tmp_query_norm) + cosine_eps)
             similarity = similarity.permute(0, 2, 1)
             similarity = F.softmax(similarity, dim=-1)
-            similarity = torch.bmm(similarity, tmp_mask.flatten(2).transpose(-2, -1)).squeeze(-1)
-            similarity = (similarity - similarity.min(1)[0].unsqueeze(1)) / (
-                    similarity.max(1)[0].unsqueeze(1) - similarity.min(1)[0].unsqueeze(1) + cosine_eps)
+            similarity = jt.bmm(similarity, tmp_mask.flatten(2).transpose(-2, -1)).squeeze(-1)
+            similarity = (similarity - similarity.min(dim=1, keepdims=True)[0]) / (
+                    similarity.max(dim=1, keepdims=True)[0] - similarity.min(dim=1, keepdims=True)[0] + cosine_eps)
             corr_query = similarity.view(bsize, 1, sp_sz, sp_sz)
             corr_query = F.interpolate(corr_query, size=fts_size, mode='bilinear', align_corners=True)
             corr_query_mask_list.append(corr_query)
-        corr_query_mask = torch.cat(corr_query_mask_list, 1)
-        corr_query_mask = (corr_query_mask).mean(1, True)
+        corr_query_mask = jt.concat(corr_query_mask_list, dim=1)
+        corr_query_mask = (corr_query_mask).mean(dim=1, keepdims=True)
         return corr_query_mask
 
     # que_img, sup_img, sup_mask, que_mask(meta), cat_idx(meta)
-    def forward(self, x, s_x, s_y, y_m, cat_idx=None):
-        x_size = x.size()  # bs, 3, 473, 473
+    def execute(self, x, s_x, s_y, y_m, cat_idx=None):
+        x_size = x.size()
         bs = x_size[0]
         h = int((x_size[2] - 1) / 8 * self.zoom_factor + 1)  # (473 - 1) / 8 * 8 + 1 = 60
         w = int((x_size[3] - 1) / 8 * self.zoom_factor + 1)  # 60
@@ -163,17 +162,17 @@ class OneModel(nn.Module):
         # Feature Extraction - Query/Support
         # ========================================
         # Query/Support Feature
-        with torch.no_grad():
+        with jt.no_grad():
             qry_bcb_fts = self.backbone(x)
             supp_bcb_fts = self.backbone(s_x.view(-1, 3, x_size[2], x_size[3]))
 
         query_feat_high = qry_bcb_fts['3']
 
-        query_feat = torch.cat([qry_bcb_fts['1'], qry_bcb_fts['2']], dim=1)
+        query_feat = jt.concat([qry_bcb_fts['1'], qry_bcb_fts['2']], dim=1)
         query_feat = self.down_query(query_feat)
         query_feat = F.interpolate(query_feat, size=size, mode='bilinear', align_corners=True)
         fts_size = query_feat.size()[-2:]
-        supp_feat = torch.cat([supp_bcb_fts['1'], supp_bcb_fts['2']], dim=1)
+        supp_feat = jt.concat([supp_bcb_fts['1'], supp_bcb_fts['2']], dim=1)
         supp_feat = self.down_supp(supp_feat)
         supp_feat = F.interpolate(supp_feat, size=size, mode='bilinear', align_corners=True)
 
@@ -194,10 +193,10 @@ class OneModel(nn.Module):
             supp_pro_list.append(supp_pro)
 
         # Support features/prototypes/masks
-        supp_mask = torch.cat(mask_list, dim=1).mean(1, True)  # bs, 1, 60, 60
-        supp_feat = torch.cat(supp_feat_list, dim=-1).mean(-1)  # bs, 256, 60, 60
-        supp_pro = torch.cat(supp_pro_list, dim=2).mean(2, True)  # bs, 256, 1, 1
-        supp_pro = supp_pro.expand_as(query_feat)  # bs, 256, 60, 60
+        supp_mask = jt.concat(mask_list, dim=1).mean(dim=1, keepdims=True)  # bs, 1, 60, 60
+        supp_feat = jt.concat(supp_feat_list, dim=-1).mean(-1)  # bs, 256, 60, 60
+        supp_pro = jt.concat(supp_pro_list, dim=2).mean(dim=2, keepdims=True)  # bs, 256, 1, 1
+        supp_pro = supp_pro.expand(query_feat.shape)  # bs, 256, 60, 60
 
         # Prior Similarity Mask
         corr_query_mask = self.generate_prior(query_feat_high, final_supp_list, mask_list, fts_size)
@@ -206,9 +205,9 @@ class OneModel(nn.Module):
         # Cross Swin Transformer
         # ========================================
         # Adapt query/support features with support prototype
-        query_cat = torch.cat([query_feat, supp_pro, corr_query_mask], dim=1)  # bs, 512, 60, 60
+        query_cat = jt.concat([query_feat, supp_pro, corr_query_mask], dim=1)  # bs, 512, 60, 60
         query_feat = self.init_merge_query(query_cat)  # bs, 256, 60, 60
-        supp_cat = torch.cat([supp_feat, supp_pro, supp_mask], dim=1)  # bs, 512, 60, 60
+        supp_cat = jt.concat([supp_feat, supp_pro, supp_mask], dim=1)  # bs, 512, 60, 60
         supp_feat = self.init_merge_supp(supp_cat)  # bs, 256, 60, 60
 
         # Swin transformer (cross)
@@ -221,7 +220,7 @@ class OneModel(nn.Module):
                     F.interpolate(qry_feat, size=(fts_size[0], fts_size[1]), mode='bilinear', align_corners=True)
                 )
             )
-        merge_feat = torch.cat(fused_query_feat, dim=1)
+        merge_feat = jt.concat(fused_query_feat, dim=1)
 
         # ========================================
         # Meta Output
@@ -240,8 +239,42 @@ class OneModel(nn.Module):
         # ========================================
         if self.training:
             main_loss = self.criterion_dice(meta_out, y_m.long())
-            aux_loss1 = torch.zeros_like(main_loss)
-            aux_loss2 = torch.zeros_like(main_loss)
-            return meta_out.max(1)[1], main_loss, aux_loss1, aux_loss2
+            aux_loss1 = jt.zeros_like(main_loss)
+            aux_loss2 = jt.zeros_like(main_loss)
+            return meta_out.argmax(dim=1)[0], main_loss, aux_loss1, aux_loss2
         else:
             return meta_out
+
+
+if __name__ == "__main__":
+    import argparse
+    from util import config
+
+    def get_parser():
+        parser = argparse.ArgumentParser(description='PyTorch Few-Shot Semantic Segmentation')
+        parser.add_argument('--arch', type=str, default='SCCAN')
+        parser.add_argument('--viz', action='store_true', default=False)
+        parser.add_argument('--config', type=str, default='config/pascal/pascal_split0_resnet50.yaml',
+                            help='config file')  # coco/coco_split0_resnet50.yaml
+        parser.add_argument('--opts', help='see config/pascal/pascal_split0_resnet50.yaml for all options', default=None,
+                            nargs=argparse.REMAINDER)
+        args = parser.parse_args()
+        assert args.config is not None
+        cfg = config.load_cfg_from_cfg_file(args.config)
+        cfg = config.merge_cfg_from_args(cfg, args)
+        if args.opts is not None:
+            cfg = config.merge_cfg_from_list(cfg, args.opts)
+        return cfg
+    
+    args = get_parser()
+    model = OneModel(args)
+    # s_x: torch.Size([1, 1, 3, 473, 473]), s_y: torch.Size([1, 1, 473, 473]), x: torch.Size([1, 3, 473, 473]), y_m: torch.Size([1, 473, 473]), cat_idx: [tensor([2])]
+    s_x = jt.randn(1, 1, 3, 473, 473)
+    s_y = jt.randn(1, 1, 473, 473)
+    x = jt.randn(1, 3, 473, 473)
+    y_m = jt.randn(1, 473, 473)
+    output = model(x, s_x, s_y, y_m)
+    print(output[0])
+    print(output[1])
+    print(output[2])
+    print(output[3])
