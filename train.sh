3303a0c2eaf050a151f8ed4291d999dafea9230b
LTL <1350378249@qq.com>
2025-07-15 09:22:20 +0800

    update: train.sh: save log file

--- train.sh
+++ train.sh
@@ -1,3 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 DEVICE_ID=$1
-CUDA_VISIBLE_DEVICES=$DEVICE_ID python train_sccan.py --config=config/pascal/pascal_split0_resnet50.yaml --viz
+SPLIT=$2
+
+mkdir -p logs
+
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+LOG_FILE="logs/train_split_${SPLIT}_${TIMESTAMP}.log"
+
+CUDA_VISIBLE_DEVICES=$DEVICE_ID python train_sccan.py \
+  --config=config/pascal/pascal_split${SPLIT}_resnet50.yaml \
+  --viz 2>&1 | tee "$LOG_FILE"
